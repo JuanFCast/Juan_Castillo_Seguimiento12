ba368d66dfad4c1c5ab0d5d20a5cc09f26d025b4
Juan Felipe Castillo Gomez <1006071586@u.icesi.edu.co>
2021-05-01 19:21:50 -0500

    Creando todas las clases

--- src/model/Instruments.java
+++ src/model/Instruments.java
@@ -1,5 +1,19 @@
 package model;
 
 public class Instruments{
+	private String idNumber;
+
+	// relaciones
+	private Dimension dimensions;
+	private Distributor distributorCompany;
+	private Place places;
+
+	//constructor
+	public Instruments(String idNumber, Dimension dimensions, Distributor distributorCompany, Place places){
+		this.idNumber = idNumber;
+		this.dimensions = dimensions;
+		this.distributorCompany = distributorCompany;
+		this.places = places;
+	}
 
 }
---
+++ src/model/Material.java
@@ -0,0 +1,6 @@
+package model;
+
+public enum Material{
+	METALES, MADERA;
+
+}
